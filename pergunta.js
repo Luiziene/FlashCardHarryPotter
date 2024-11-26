0d18f9f17576d5ee55f08cb2d4f60a31dc7d90d3
Luiziene <148452203+Luiziene@users.noreply.github.com>
2024-11-26 15:01:15 -0300

    versão Harry Potter

---
+++ pergunta.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Harry Potter',
+    'Para que se usa o feitiço Alohomora?',
+    'Abrir uma porta'
+)
+
+criaCartao(
+    'Harry Potter',
+    'Quantas Pedras Filosofais existem?',
+    'Uma'
+)
+
+criaCartao(
+    'Harry Potter',
+    'Quem era o dono original do Chapéu Seletor?',
+    'Godric Gryffindor'
+)
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o fantasma da Lufa-Lufa?',
+    'O Frei Gorducho'
+)
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o zelador de Hogwarts?',
+    'Filch'
+)
+
+criaCartao(
+    'Harry Potter',
+    'Como você cumprimenta um Hipogrifo?',
+    'Com uma reverência'
+)
+
+criaCartao(
+    'Harry Potter',
+    'Quem é o Guardião das Chaves de Hogwarts?',
+    'Rubeus Hagrid'
+)
+
+criaCartao(
+    'Harry Potter',
+    'Qual planta te ajuda a respirar debaixo d’água?',
+    'Guelricho'
+)
+
+criaCartao(
+    'Harry Potter',
+    'Quem escreveu Harry Potter?',
+    'JK Rowling'
+)
+
+criaCartao(
+    'Harry Potter',
+    'Quantos alunos dormem em cada dormitório?',
+    '5'
+)
+
+criaCartao(
+    'Harry Potter',
+    'Professor Snape é o diretor de qual casa de Hogwarts?',
+    'Sonserina'
+)
+
+criaCartao(
+    'Harry Potter',
+    'Qual feitiço tem o efeito oposto ao Lumus?',
+    'Nox'
+)
